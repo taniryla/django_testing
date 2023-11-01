5c12fce8cd68b2f8ba21402b6e07d660279f553f
Chako <ryland0000@gmail.com>
2023-11-01 12:32:11 -0700

    integration testing 3

--- app/tutorials/tests.py
+++ app/tutorials/tests.py
@@ -26,3 +26,16 @@ def test_update_tutorial(new_tutorial):
     new_tutorial.title = 'Pytest-Django'
     new_tutorial.save()
     assert Tutorial.objects.filter(title='Pytest-Django').exists()
+
+@pytest.fixture
+def another_tutorial(db):
+    tutorial = Tutorial.objects.create(
+        title='More-Pytest',
+        tutorial_url='https://pytest-django.readthedocs.io/en/latest/index.html',
+        description='Tutorial on how to apply pytest to a Django application',
+        published=True
+    )
+    return tutorial
+
+def test_compare_tutorials(new_tutorial, another_tutorial):
+    assert new_tutorial.pk != another_tutorial.pk
