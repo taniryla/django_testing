279c341068975f68c51b3bda3f47d22689d2158e
Chako <ryland0000@gmail.com>
2023-11-01 12:25:59 -0700

    integration testing 2

--- app/tutorials/tests.py
+++ app/tutorials/tests.py
@@ -9,12 +9,20 @@ def test_homepage_access():
     url = reverse('home')
     assert url == "/"
 
-@pytest.mark.django_db 
-def test_create_tutorial():
+@pytest.fixture
+def new_tutorial(db):
     tutorial = Tutorial.objects.create(
         title='Pytest',
         tutorial_url='https://pytest-django.readthedocs.io/en/latest/index.html',
         description='Tutorial on how to apply pytest to a Django application',
         published=True
     )
-    assert tutorial.title == "Pytest"
+    return tutorial
+
+def test_search_tutorials(new_tutorial):
+    assert Tutorial.objects.filter(title='Pytest').exists()
+
+def test_update_tutorial(new_tutorial):
+    new_tutorial.title = 'Pytest-Django'
+    new_tutorial.save()
+    assert Tutorial.objects.filter(title='Pytest-Django').exists()
